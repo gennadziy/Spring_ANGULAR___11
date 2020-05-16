4bcce65b98b5e444c8fad99641a5bcf16c21005f
gennadziy <geniy@tut.by>
2020-05-16 19:08:09 +0200

    make it better

--- src/main/java/Spring_ANGULAR/Spring_ANGULAR/EmployeeController.java
+++ src/main/java/Spring_ANGULAR/Spring_ANGULAR/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://app-gennadziy.herokuapp.com/")
 @RestController
 @RequestMapping("/springboot-crud-rest/api/v1")
 public class EmployeeController {
