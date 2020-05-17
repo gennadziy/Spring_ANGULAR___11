1ad3aed0759e722c2190bccee66eed0ae7405cb0
gennadziy <geniy@tut.by>
2020-05-17 20:23:43 +0200

    make it better

--- src/main/java/Spring_ANGULAR/Spring_ANGULAR/repository/PlanetDao.java
+++ src/main/java/Spring_ANGULAR/Spring_ANGULAR/repository/PlanetDao.java
@@ -3,6 +3,7 @@ package Spring_ANGULAR.Spring_ANGULAR.repository;
 
 import Spring_ANGULAR.Spring_ANGULAR.Planets;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /*
 @Author Gennadziy GITHUB/gennadziy
@@ -10,6 +11,6 @@ Class name: PlanetDao
 Date: 2020-05-13
 Time: 20:30
 */
-
+@Repository
 public interface  PlanetDao extends JpaRepository<Planets, Integer> {
 }
