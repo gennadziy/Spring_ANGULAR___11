0d3a7e23a50e4b154ea2475079ea80a586ee8d66
gennadziy <geniy@tut.by>
2020-05-24 13:44:51 +0200

    make it better

---
+++ src/main/java/Spring_ANGULAR/Spring_ANGULAR/testsss.java
@@ -0,0 +1,27 @@
+package Spring_ANGULAR.Spring_ANGULAR;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+@Author Gennadziy GITHUB/gennadziy
+Class name: testsss
+Date: 2020-05-21
+Time: 22:33
+*/
+public class testsss {
+
+    public static void main ( String[] args ) {
+        List<Integer> list =new ArrayList <> (  );
+        list.add(232);
+        list.add(232);list.add(232);
+        list.add(232);
+        list.add(232);
+
+        list.forEach ( a-> System.out.println (a ) );
+      for(Integer i: list){
+          System.out.println (i );
+      }
+
+    }
+}
